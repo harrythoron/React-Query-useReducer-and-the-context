309473ca9a9a0c728335ad6c1e24b1c65c6a28f3
throw226th <haritharan98@siswa.um.edu.my>
2023-11-08 22:26:54 +0800

    final changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,72 @@
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query"
+import { getNotes, createNote, updateNote } from "./requests"
+
 const App = () => {
+  const queryClient = useQueryClient()
+  const newNoteMutation = useMutation({
+    mutationFn: createNote,
+    onSuccess: (newNote) => {
+      // incase to optimize performance manually update the query state maintained by react query
+      const notes = queryClient.getQueryData(['notes'])
+      queryClient.setQueryData(['notes'], notes.concat(newNote))
+
+
+      // queryClient.invalidateQueries({ queryKey: ['notes'] })
+    }
+  })
+  const toggleMutation = useMutation({
+    mutationFn: updateNote,
+    onSuccess: (changedNote) => {
+      // incase to optimize performance manually update the query state maintained by react query
+      const notes = queryClient.getQueryData(['notes'])
+      queryClient.setQueryData(['notes'], notes.map(note => note.id !== changedNote.id ? note : changedNote))
+
+
+      // queryClient.invalidateQueries({ queryKey: ['notes'] })
+    }
+  })
+
   const addNote = async (event) => {
     event.preventDefault()
     const content = event.target.note.value
     event.target.note.value = ''
-    console.log(content)
+    newNoteMutation.mutate({ content, important: false })
   }
 
   const toggleImportance = (note) => {
-    console.log('toggle importance of', note.id)
+    const changedNote = {
+      ...note,
+      important: !note.important
+    }
+    toggleMutation.mutate(changedNote)
   }
 
-  const notes = []
+  const result = useQuery({
+    queryKey: ['notes'],
+    queryFn: getNotes,
+    refetchOnWindowFocus: false
+    // queryFn: () => axios.get('http://localhost:3001/notes').then(res => {
+    //   // console.log(res.data)
+    //   return res.data
+    // })
+  })
+  // console.log(result, 'before')
+  console.log(JSON.parse(JSON.stringify(result)))
 
-  return(
+  if (result.isLoading) {
+    return <div>
+      <h2>Notes app</h2>
+      <form >
+        <input name="note" />
+        <button type="submit">add</button>
+      </form>
+      loading data...
+    </div>
+  }
+
+  const notes = result.data
+
+  return (
     <div>
       <h2>Notes app</h2>
       <form onSubmit={addNote}>
@@ -21,7 +75,7 @@ const App = () => {
       </form>
       {notes.map(note =>
         <li key={note.id} onClick={() => toggleImportance(note)}>
-          {note.content} 
+          {note.content}
           <strong> {note.important ? 'important' : ''}</strong>
         </li>
       )}
